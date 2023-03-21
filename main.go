3556e090ed985690a079dffd9152921c925cae2c
Le_chat <notAlyosha@bk.ru>
2023-03-21 12:24:43 +0300

    almost done

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	f "./funcs"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"os"
 )
 
 func main() {
-	f.BlackAndWhite()
+	if len(os.Args) < 1 {
+		panic("I need path to image")
+	}
+
+	path := os.Args[1]
+	imgIn, _ := os.Open(path)
+	img, _ := jpeg.Decode(imgIn)
+
+	imgOut := image.NewGray16(img.Bounds().Bounds())
+
+	for y := 0; y < imgOut.Bounds().Dy(); y += 1 {
+		for x := 0; x < imgOut.Bounds().Dx(); x += 1 {
+			currentPixel := img.At(x, y)
+			r, g, b, _ := currentPixel.RGBA()
+
+			gray := color.Gray16{uint16((r + g + b) / 3)}
+			imgOut.SetGray16(x, y, gray)
+		}
+	}
+
+	f, _ := os.Create("b")
+	jpeg.Encode(f, imgOut, &jpeg.Options{Quality: 16})
 }
